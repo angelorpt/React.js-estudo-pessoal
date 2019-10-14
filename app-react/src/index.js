b76e126588278680e36247d2a3de7abe00512250
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2019-10-14 16:52:32 -0400

    Eventos no React

--- app-react/src/index.js
+++ app-react/src/index.js
@@ -7,9 +7,10 @@ import Clock from './Clock';
 import Clock2 from './Clock2';
 import Props from './Props';
 import State from './State';
+import Events from './Events';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Clock2 />, document.getElementById('root'));
+ReactDOM.render(<Events />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
